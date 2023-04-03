08aad0e7763a062974cec615e16bcbff099f6f86
Gesiel Gimenez <gesielgimenez@gmail.com>
2023-04-03 15:47:29 -0500

    declaracion de variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//alert("estoy enlazada")
+
+let pokedex = []; //creamos un array vacio
+
+const URL_API = "https://pokeapi.co/api/v2/pokemon"; //API
+//const boxButtons = document.getElementById("lado1");//
+const containerPoke = document.getElementById("contenedorpokemon"); //captura del contenedor
+
