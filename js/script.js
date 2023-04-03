236c1619fce40214d52516dcec93112142e56c58
linavaquero <lina.vaquero2017@gmail.com>
2023-04-03 16:51:52 -0500

    enlace axios

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+const getPokemons = async (url) => {
+    const {data} = await axios.get(url);
+    console.log("api" , data.game_indices);
+    return data;
+}
+getPokemons ("https://pokeapi.co/api/v2/pokemon/ditto")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const otrosPokemon = docume
+.addEventListener('click', () => {
+    printsVideos(containersCards, videoTube)
+})
